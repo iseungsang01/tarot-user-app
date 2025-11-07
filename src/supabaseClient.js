80288287f5e30fad90e237fe442c0893c074089d
iseungsang01 <lss010330@snu.ac.kr>
2025-11-07 21:15:20 +0900

    Export AdminPassWord along with supabase client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
-const AdminPassWord = process.env. REACT_APP_ADMIN_PASSWORD
+const AdminPassWord = process.env.REACT_APP_ADMIN_PASSWORD
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export {AdminPassWord, supabase}
